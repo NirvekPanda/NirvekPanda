77798ba4556c06f06fda5f9d4e84076712a58a87
Nirvek <nirvekpandey@gmail.com>
2024-05-12 20:48:05 -0700

    updates

---
+++ front/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>Under Construction</code>.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/NirvekPandey"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Contact me on Linkedin for more info
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
